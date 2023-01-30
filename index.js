19d69a166e799940b7d67ca5df2b5c459bd1893c
mohan8git <bishnoimohan502@gmail.com>
2023-01-30 17:49:46 +0530

    cors added

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ InitiateMongoServer();
 
 const app = express();
 app.use(cors());
+// allow cross-origin requests
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // PORT
 const PORT = process.env.PORT || 4000;
